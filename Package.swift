67bf60f7dd24e1fad654a341a2ae935b2012daa3
izabekz <izabekz@gmail.com>
2022-06-07 23:49:59 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUICharts2",
-            targets: ["SwiftUICharts"]),
+            targets: ["SwiftUICharts2"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUICharts",
+            name: "SwiftUICharts2",
             dependencies: []),
         .testTarget(
             name: "SwiftUIChartsTests",
-            dependencies: ["SwiftUICharts"]),
+            dependencies: ["SwiftUICharts2"]),
     ]
 )
