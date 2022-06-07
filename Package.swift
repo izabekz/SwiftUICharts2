9d979bbe552ad26ff4072bdba395140b7fcb6dd8
izabekz <izabekz@gmail.com>
2022-06-07 23:47:38 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUICharts",
+            name: "SwiftUICharts2",
             targets: ["SwiftUICharts"]),
     ],
     dependencies: [
